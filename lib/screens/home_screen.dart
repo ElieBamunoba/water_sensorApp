e51ee32dd1763d4d3553d8b935874746829dca11
ElieBamunoba <eliebamunoba@gmail.com>
2022-02-17 17:46:35 +0300

    init

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,70 +1,57 @@
 import 'package:flutter/material.dart';
-import 'package:percent_indicator/circular_percent_indicator.dart';
 
-class HomeScreen extends StatefulWidget {
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
+import '../widgets/waveClipper.dart';
 
-class _HomeScreenState extends State<HomeScreen> {
-  double currentValue = 0.53;
-
-  void updateValue({required double newValue}) {
-    setState(() {
-      currentValue = newValue;
-    });
-  }
+class MyHomeScreen extends StatelessWidget {
+  const MyHomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          "Water Sensor",
-        ),
-        centerTitle: true,
-        // ignore: prefer_const_literals_to_create_immutables
-        elevation: 0.0,
-        // ignore: prefer_const_literals_to_create_immutables
-        actions: [
-          PopupMenuButton(
-            icon: const Icon(Icons.more_vert),
-            onSelected: null,
-            itemBuilder: (_) => [
-              const PopupMenuItem(
-                child: Text("About Us"),
-                onTap: null, //function to opern about us page,
-              ),
-            ],
-          )
-        ],
-      ),
-      body: Center(
-        child: Container(
-          height: MediaQuery.of(context).size.height * 50 / 100,
-          child: Column(
+          centerTitle: true,
+          title: const Text("Water Sensor"),
+          backgroundColor: Colors.redAccent,
+          actions: [
+            PopupMenuButton(
+              icon: const Icon(Icons.more_vert),
+              onSelected: null,
+              itemBuilder: (_) => [
+                const PopupMenuItem(
+                  child: Text("About Us"),
+                  onTap: null, //function to opern about us page,
+                ),
+              ],
+            )
+          ]),
+      body: Column(
+        children: [
+          Stack(
             children: [
-              CircularPercentIndicator(
-                radius: 90,
-                center: Text("${currentValue * 100} %"),
-                animateFromLastPercent: true,
-                animationDuration: 5,
-                backgroundWidth: 15,
-                percent: currentValue,
-                backgroundColor: Colors.grey,
-                progressColor: currentValue > 0.5 ? Colors.blue : Colors.orange,
+              Opacity(
+                opacity: 0.5,
+                child: ClipPath(
+                  clipper: WaveClipper(),
+                  child: Container(
+                    color: Colors.deepOrangeAccent,
+                    height: 200,
+                  ),
+                ),
               ),
-              const SizedBox(
-                height: 30,
+              ClipPath(
+                clipper: WaveClipper(),
+                child: Container(
+                  color: Colors.red,
+                  height: 180,
+                  alignment: Alignment.center,
+                ),
               ),
-              const Text("Amount of water"),
-              // ElevatedButton(
-              //     onPressed: () => updateValue(newValue: 0.51),
-              //     child: Text("Add"))
             ],
           ),
-        ),
+        ],
       ),
     );
   }
 }
+
+//Costom CLipper class with Path
